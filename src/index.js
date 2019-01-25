b10fea42a95205973903b860612efe123e32dad0
kikesan <kikin1601@hotmail.com>
2019-01-25 12:52:13 -0500

    Read from firebase success!

--- src/index.js
+++ src/index.js
@@ -5,12 +5,33 @@ import { Provider } from 'react-redux'
 
 import App from './App'
 import reducers from './reducers'
+import firebase from 'firebase'
+
+var config = {
+	apiKey: 'AIzaSyAxt-MnqrVcRWrPeAcWYt_ApueQIuiokUU',
+	authDomain: 'todoapp-c0094.firebaseapp.com',
+	databaseURL: 'https://todoapp-c0094.firebaseio.com',
+	projectId: 'todoapp-c0094',
+	storageBucket: 'todoapp-c0094.appspot.com',
+	messagingSenderId: '372140339810'
+}
+var defaultApp = firebase.initializeApp(config)
+var database = firebase.database()
+console.log(defaultApp.name)
+var starCountRef = firebase.database().ref('task/')
+starCountRef.on('value', function(snapshot) {
+	//updateStarCount(postElement, snapshot.val());
+	console.log('result:::', snapshot.val())
+})
 
 const store = createStore(reducers)
 
 const root = document.getElementById('root')
-root ? ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-  , root) : false
+root
+	? ReactDOM.render(
+			<Provider store={store}>
+				<App />
+			</Provider>,
+			root
+	  )
+	: false
